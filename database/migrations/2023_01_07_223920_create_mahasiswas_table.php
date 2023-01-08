9b17da1ddee189c01fb09d0b58e090aef293c808
AZIS NUR FAJAR <azisnurfajar32@gmail.com>
2023-01-08 09:00:47 +0700

    update

---
+++ database/migrations/2023_01_07_223920_create_mahasiswas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim');
+            $table->text('nama');
+            $table->text('jenis_kelamin');
+            $table->text('alamat');
+            $table->text('prodi');
+            $table->text('semester');
+            $table->text('kelas');
+            $table->text('tahun_angkatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
